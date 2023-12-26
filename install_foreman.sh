5ee7d3c012069becc1c9fd9ef86e29e7a5c15c49
shazadw1 <70218450+shazadw1@users.noreply.github.com>
2023-12-26 13:06:00 +0000

    Create install_foreman.sh

---
+++ install_foreman.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Install required packages
+sudo apt update​
+
+# Install required packages
+sudo apt install -y apt-transport-https wget gpg gnupg ca-certificates
+
+# Download Puppet repository
+wget https://apt.puppet.com/puppet7-release-focal.deb
+
+# Add Puppet repository
+sudo dpkg -i ./puppet7-release-focal.deb
+
+# Add Foreman repository to system
+echo "deb http://deb.theforeman.org/ focal 3.3" | sudo tee /etc/apt/sources.list.d/foreman.list
+echo "deb http://deb.theforeman.org/ plugins 3.3" | sudo tee -a /etc/apt/sources.list.d/foreman.list
+
+# Download Foreman GPG keys
+wget -q https://deb.theforeman.org/pubkey.gpg -O- | sudo apt-key add -
+
+# Update system package list
+sudo apt update
+
+# Install Foreman
+sudo apt -y install foreman-installer
+
+# Run Foreman installation
+foreman-installer
