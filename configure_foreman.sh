f7c97e8689b442f1f60c45f04d90b77c8f8faa21
shazadw1 <70218450+shazadw1@users.noreply.github.com>
2023-12-26 19:00:36 +0000

    Update configure_foreman.sh

--- configure_foreman.sh
+++ configure_foreman.sh
@@ -19,30 +19,13 @@ generate_proxy_key() {
     proxy_home=$(getent passwd foreman-proxy | cut -d: -f6)
 
     # Check if .ssh exists and is a file, then remove it
-    if [ -f "$proxy_home/.ssh" ]; then
-        echo "Found a file named .ssh, removing it..."
-        rm -f "$proxy_home/.ssh"
-    fi
+    rm -f "$proxy_home/.ssh"
 
     # Check if the .ssh directory exists, if not create it
-    if [ ! -d "$proxy_home/.ssh" ]; then
-        echo "Creating .ssh directory..."
-        mkdir "$proxy_home/.ssh"
-        chown foreman-proxy:foreman-proxy "$proxy_home/.ssh"
-    else
-        echo ".ssh directory already exists."
-    fi
-
-    # Generate SSH key without a passphrase, if it doesn't already exist
-    if [ ! -f "$proxy_home/.ssh/id_rsa_foreman_proxy" ]; then
-        sudo -u foreman-proxy ssh-keygen -f "$proxy_home/.ssh/id_rsa_foreman_proxy" -N ''
-    else
-        echo "SSH key already exists."
-    fi
+     mkdir "$proxy_home/.ssh"
+     # Generate SSH key without a passphrase, if it doesn't already exist
+    sudo -u foreman-proxy ssh-keygen -f "$proxy_home/.ssh/id_rsa_foreman_proxy" -N ''
 }
-
-# Main script execution
-echo "Starting script..."
 
 # Function to update SSL
 update_ssl() {
@@ -60,24 +43,16 @@ update_ssl() {
     log "Running Certbot command: $certbot_command"
     $certbot_command
 
-    # Check the exit status of Certbot command
-    if [ $? -ne 0 ]; then
-        log "Error: Certbot command failed. Please check Certbot logs for details."
-        exit 1
-    fi
-
-    log "Certbot command completed successfully."
-
     # Check if the Let's Encrypt files exist and update Foreman configurations
     local cert_file="/etc/letsencrypt/live/$HOSTNAME/cert.pem"
     local chain_file="/etc/letsencrypt/live/$HOSTNAME/chain.pem"
     local key_file="/etc/letsencrypt/live/$HOSTNAME/privkey.pem"
-    local ca_bundle="/etc/ssl/certs/ca-bundle.crt"
+   # local ca_bundle="/etc/ssl/certs/ca-bundle.crt"
 
     [[ -f "$cert_file" ]] && sudo foreman-installer --foreman-server-ssl-cert "$cert_file" || echo "Certificate file not found."
     [[ -f "$chain_file" ]] && sudo foreman-installer --foreman-server-ssl-chain "$chain_file" || echo "Chain file not found."
     [[ -f "$key_file" ]] && sudo foreman-installer --foreman-server-ssl-key "$key_file" || echo "Key file not found."
-    [[ -f "$ca_bundle" ]] && sudo foreman-installer --foreman-proxy-foreman-ssl-ca "$ca_bundle" --puppet-server-foreman-ssl-ca "$ca_bundle" || echo "CA bundle file not found."
+    #[[ -f "$ca_bundle" ]] && sudo foreman-installer --foreman-proxy-foreman-ssl-ca "$ca_bundle" --puppet-server-foreman-ssl-ca "$ca_bundle" || echo "CA bundle file not found."
 }
 
 # Main script execution
