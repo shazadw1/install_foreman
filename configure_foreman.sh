1bc9160bcebfd8d17594286441551f971424a09e
shazadw1 <70218450+shazadw1@users.noreply.github.com>
2023-12-26 14:11:02 +0000

    Update configure_foreman.sh

--- configure_foreman.sh
+++ configure_foreman.sh
@@ -1,1 +1,57 @@
+#!/bin/bash
+
+# Function to install plugins
+install_plugins() {
+    echo "Installing plugins..."
+
+    # Install Foreman Remote Execution plugin
+    sudo apt-get install ruby-foreman-remote-execution
+
+    # Install Smart Proxy Remote Execution SSH plugin
+    sudo apt-get install ruby-smart-proxy-remote-execution-ssh
+
+    # Enable Foreman plugin templates
+    sudo foreman-installer --enable-foreman-plugin-templates
+}
+
+# Function to update SSL
+update_ssl() {
+    echo "Updating SSL..."
+
+    # Get the actual hostname of the system
+    HOSTNAME=$(hostname)
+
+    # Install Certbot and its Apache plugin
+    sudo apt-get install -y certbot python3-certbot-apache
+
+    # Generate SSL certificate
+    sudo certbot certonly -d $HOSTNAME --webroot /var/lib/foreman/public
+
+    # Check if the Let's Encrypt files exist and update Foreman configurations
+    local cert_file="/etc/letsencrypt/live/$HOSTNAME/cert.pem"
+    local chain_file="/etc/letsencrypt/live/$HOSTNAME/chain.pem"
+    local key_file="/etc/letsencrypt/live/$HOSTNAME/privkey.pem"
+    local ca_bundle="/etc/ssl/certs/ca-bundle.crt"
+
+    [[ -f "$cert_file" ]] && sudo foreman-installer --foreman-server-ssl-cert "$cert_file" || echo "Certificate file not found."
+    [[ -f "$chain_file" ]] && sudo foreman-installer --foreman-server-ssl-chain "$chain_file" || echo "Chain file not found."
+    [[ -f "$key_file" ]] && sudo foreman-installer --foreman-server-ssl-key "$key_file" || echo "Key file not found."
+    [[ -f "$ca_bundle" ]] && sudo foreman-installer --foreman-proxy-foreman-ssl-ca "$ca_bundle" --puppet-server-foreman-ssl-ca "$ca_bundle" || echo "CA bundle file not found."
+}
+
+# Function to perform a backup
+backup() {
+    echo "Performing backup..."
+    # Backup commands...
+}
+
+# Main script execution
+echo "Starting script..."
+
+# Call functions
+install_plugins
+update_ssl
+backup
+
+echo "Script execution completed."
 
