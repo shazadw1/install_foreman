7d0bc81e9cbfdd1da449f97956daea8df5814344
shazadw1 <70218450+shazadw1@users.noreply.github.com>
2024-01-01 03:18:19 +0000

    Update install_foreman.sh

--- install_foreman.sh
+++ install_foreman.sh
@@ -10,14 +10,16 @@ sudo hostnamectl set-hostname sysop.adaplo.co.uk
 sudo apt install -y apt-transport-https wget gpg gnupg ca-certificates
 
 # Download Puppet repository
-wget https://apt.puppet.com/puppet7-release-focal.deb
+#wget https://apt.puppet.com/puppet7-release-focal.deb
+wget https://apt.puppet.com/puppet8-release-focal.deb
 
 # Add Puppet repository
-sudo dpkg -i ./puppet7-release-focal.deb
+#sudo dpkg -i ./puppet7-release-focal.deb
+sudo dpkg -i ./puppet8-release-focal.deb
 
 # Add Foreman repository to system
-echo "deb http://deb.theforeman.org/ focal 3.4" | sudo tee /etc/apt/sources.list.d/foreman.list
-echo "deb http://deb.theforeman.org/ plugins 3.4" | sudo tee -a /etc/apt/sources.list.d/foreman.list
+echo "deb http://deb.theforeman.org/ focal 3.5" | sudo tee /etc/apt/sources.list.d/foreman.list
+echo "deb http://deb.theforeman.org/ plugins 3.5" | sudo tee -a /etc/apt/sources.list.d/foreman.list
 
 # Download Foreman GPG keys
 wget -q https://deb.theforeman.org/pubkey.gpg -O- | sudo apt-key add -
